bab78a76c16f3a3f2bfd3db57020388f646c2065
ZachSParker <evergreenzach@gmail.com>
2023-08-20 20:11:47 -0600

    finished betterThanAverage function

--- arrayChallenges/script.js
+++ arrayChallenges/script.js
@@ -12,6 +12,36 @@ function alwaysHungry(arr){
         console.log("I'm Hungry")
     }
 }
-function highPass(){
+function highPass(arr,cutoff){
+    var filteredArr =[];
+    for(var i=0;i<arr.length;i++){
+        if(arr[i] > cutoff)
+        {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+// var result = highPass([6.8,3,10,-2,5,9],5)
+// console.log(result);
+function betterThanAverage(arr){
+    var sum = 0;
+    var average = 0;
+    var count = 0;
+    for(var i = 0;i<arr.length;i++){
+        sum+= i;
+    }
+    average = sum / arr.length;
+    for(var i = 1;i<arr.length;i++){
+        if(arr[i] > average){
+            count++;
+        }
+    }
+    return count;
+}
+// var result = betterThanAverage([6,8,3,10,-2,5,9]);
+// console.log(result);
+function reverse(arr){
     
+    return arr;
 }
